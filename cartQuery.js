b159d641c4e902697885893cade7de50df4b2a57
sheikmagizh12 <sheik@mntfuture.com>
2026-05-07 13:20:52 +0530

    refactor: remove legacy backend project files and configuration

--- cartQuery.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-    const carts = await prisma.cartItem.findMany({});
-    console.log(carts);
-    prisma.$disconnect();
-}
-main();
